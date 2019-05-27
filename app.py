d45baf4c9714d1c878b81e7e110d4d48cd00fff5
craig ball <craig_5050@msn.com>
2019-05-27 21:31:10 +0000

    connected DB to app

--- app.py
+++ app.py
@@ -3,17 +3,23 @@ from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
+
 app = Flask(__name__)
-app.config["MONGO_DBNAME"] = 'cookbook'
+app.config["MONGO_DBNAME"] = "book"
 app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
 
 mongo = PyMongo(app)
 
 @app.route('/')
 @app.route('/home')
-def get_recipes():
-    return render_template("home.html", 
-                           recipes=mongo.db.categories.find())
+def home():
+    return render_template("home.html",
+            recipes=mongo.db.recipe.find()) 
+    
+@app.route('/addrecipe')
+def addrecipe():
+    return render_template("add_recipe.html")
+                           
                            
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
